d71bd5d863abde4b5a9f9e583c1cb6bd606597dd
Lara Sanders <larasanders@gmail.com>
2018-01-06 16:46:26 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,12 @@ class ApplicationController < Sinatra::Base
 		enable :sessions
 		set :session_secret, "password_security"
 	end
+
+	post "/signup" do
+    user = User.new(:username => params[:username], :password => params[:password])
+		if user.save
+			redirect "/login"
+	else
+			redirect "/failure"
+end
 end
