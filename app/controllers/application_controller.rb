3fe0032f9f852a5172ea595a5c3276b68ab984fa
Lara Sanders <larasanders@gmail.com>
2018-01-06 16:50:35 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,15 +8,45 @@ class ApplicationController < Sinatra::Base
 		set :session_secret, "password_security"
 	end
 
-	post '/signup' do
-    user = User.new(:username => params[:username], :password => params[:password])
-		if user.save
+	get "/" do
+		erb :index
+	end
+
+	get "/signup" do
+		erb :signup
+	end
+
+	post "/signup" do
+		#your code here!
+	end
+
+
+	get "/login" do
+		erb :login
+	end
+
+	post "/login" do
+		#your code here!
+	end
+
+	get "/success" do
+		if logged_in?
+			erb :success
+		else
 			redirect "/login"
-	else
-			redirect "/failure"
-end
-end
-helpers do
+		end
+	end
+
+	get "/failure" do
+		erb :failure
+	end
+
+	get "/logout" do
+		session.clear
+		redirect "/"
+	end
+
+	helpers do
 		def logged_in?
 			!!session[:user_id]
 		end
@@ -25,4 +55,5 @@ helpers do
 			User.find(session[:user_id])
 		end
 	end
+
 end
